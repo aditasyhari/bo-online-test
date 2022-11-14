e3b45c9e0ef2d4da6f3e59118c5b1d6972379419
aditasyhari <adit.asyhari16@gmail.com>
2022-11-14 21:18:02 +0700

    rev

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
 let url_lokal = window.location.origin;
 
+function formatRupiah(angka, prefix) 
+{
+    var number_string = angka.replace(/[^,\d]/g, "").toString(),
+        split = number_string.split(","),
+        sisa = split[0].length % 3,
+        rupiah = split[0].substr(0, sisa),
+        ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+    if (ribuan) {
+        separator = sisa ? "." : "";
+        rupiah += separator + ribuan.join(".");
+    }
+
+    rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
+    return prefix == undefined ? rupiah : rupiah ? "Rp " + rupiah : "";
+}
+
 function loadingStart() {
     document.getElementById("loader-screen").classList.add("show");
 }
@@ -149,21 +166,4 @@ function setValueFilter(className) {
         $(this).parent().find('[name=value-' + className + ']').val(valOption);
         loadDataTable();
     });
-}
-
-function formatRupiah(angka, prefix) 
-{
-    var number_string = angka.replace(/[^,\d]/g, "").toString(),
-        split = number_string.split(","),
-        sisa = split[0].length % 3,
-        rupiah = split[0].substr(0, sisa),
-        ribuan = split[0].substr(sisa).match(/\d{3}/gi);
-
-    if (ribuan) {
-        separator = sisa ? "." : "";
-        rupiah += separator + ribuan.join(".");
-    }
-
-    rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
-    return prefix == undefined ? rupiah : rupiah ? "Rp " + rupiah : "";
 }
